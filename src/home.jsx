b8101b517531f3090f325b174347eb4291e383f2
Karsten Jepsen <kjepsen86@gmail.com>
2021-11-18 22:54:26 -0600

    Changed project links to colorful buttons

--- src/home.jsx
+++ src/home.jsx
@@ -33,6 +33,8 @@ import './home.css';
 import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
+import Button from '@mui/material/Button';
+
 
 
 
@@ -123,7 +125,15 @@ export default function Home() {
                 :
                 <p className="text"><i>React - Redux - Sagas - Node - Express - Postgresql - Material UI - CSS - Moment.js</i></p>
                 }
-                <p className="text"><a href="https://boiling-plains-09195.herokuapp.com/#/home">Check it out!</a></p>
+                <Button
+                    className="text"
+                    style={{ marginLeft: '6vh', color: 'black', fontFamily: 'BioRhyme'}}
+                    variant="contained" 
+                    color="success"
+                    onClick={() => window.location.href="https://boiling-plains-09195.herokuapp.com/#/home"}
+                >
+                    Check it out!
+                </Button>
                 <img src={wh} alt="me" className="wh" style={{width: '15vh'}}/>
             </div> 
             <div className="tech">
@@ -138,8 +148,16 @@ export default function Home() {
                 :
                 <p className="text"><i>React - Redux - Sagas - Node - Express - CSV - Postgresql - Material UI </i></p>
                 }
-                <p className="text"><a href="https://safe-plains-34698.herokuapp.com/#/home">Have a look!</a>
-                    <img src={oel} alt="oel" className="oel" style={{width: '15vh'}}/></p>
+                <Button
+                    className="text"
+                    style={{ marginLeft: '6vh', color: 'black', fontFamily: 'BioRhyme'}}
+                    variant="contained" 
+                    color="primary"
+                    onClick={() => window.location.href="https://safe-plains-34698.herokuapp.com/#/home"}
+                >
+                    Have a look!
+                </Button>
+                <img src={oel} alt="oel" className="oel" style={{width: '15vh'}}/>
             </div>
             <div className="tech">
                 <h3 className="subtitles">To Do List</h3>
@@ -152,8 +170,16 @@ export default function Home() {
                 :
                 <p className="text"><i>jQuery - JavaScript - Ajax - Node - Express - Postgresql - HTML - CSS </i></p>
                 }
-                <p className="text"><a href="https://dry-shore-54453.herokuapp.com/">Take a gander!</a>
-                    <img src={todolist} alt="todolist" className="todo" style={{width: '15vh'}}/></p>
+                <Button
+                    className="text"
+                    style={{ marginLeft: '3vh', color: 'black', fontFamily: 'BioRhyme'}}
+                    variant="contained" 
+                    color="secondary"
+                    onClick={() => window.location.href="https://dry-shore-54453.herokuapp.com/"}
+                >
+                    Take a gander!
+                </Button>
+                <img src={todolist} alt="todolist" className="todo" style={{width: '15vh'}}/>
             </div>
             <div className="tech">
                 <h3 className="subtitles">Movie Gallery</h3>
@@ -166,8 +192,16 @@ export default function Home() {
                 :
                 <p className="text"><i>React - JavaScript - Redux - Saga - Node - Express - Postgresql - Material UI/CSS </i></p>
                 }
-                <p className="text"><a href="https://afternoon-falls-30309.herokuapp.com/#/">Open it up!</a>
-                    <img src={film} alt="film" className="film" style={{width: '15vh'}}/></p>
+                <Button
+                    className="text"
+                    style={{ marginLeft: '8vh', color: 'black', fontFamily: 'BioRhyme'}}
+                    variant="contained" 
+                    color="error"
+                    onClick={() => window.location.href="https://afternoon-falls-30309.herokuapp.com/#/"}
+                >
+                    Open it up!
+                </Button>
+                <img src={film} alt="film" className="film" style={{width: '15vh'}}/>
             </div>
             {/* <div class="tech">
                 <h3 class="subtitles">Photo Gallery</h3>
