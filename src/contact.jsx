5568a7e67a0511efcab6e75649c18cf473ded1ce
Karsten Jepsen <kjepsen86@gmail.com>
2021-11-28 21:22:13 -0600

    home view now fully responsive on mobile

--- src/contact.jsx
+++ src/contact.jsx
@@ -98,6 +98,7 @@ export default function Contact() {
                             label="Name"
                             variant="outlined"
                             size="small"
+                            color="success"
                             style={{ width: '95%' }}
                             value={name}
                             onChange={(e) => setName(e.target.value)}
@@ -109,6 +110,7 @@ export default function Contact() {
                             // Email Input 
                             label="Email"
                             variant="outlined"
+                            color="secondary"
                             size="small"
                             style={{ width: '100%' }}
                             value={email}
