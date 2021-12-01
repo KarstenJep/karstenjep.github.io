185fb397edbeab4b92f7be71684531cc75c9c195
Karsten Jepsen <kjepsen86@gmail.com>
2021-11-30 21:35:39 -0600

    installed useref, added functionality to scroll buttons, hover

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useRef} from 'react';
 // M-UI
 import ImageList from '@mui/material/ImageList';
 // Files
@@ -6,33 +6,44 @@ import Projects from './Projects.jsx';
 import "./Portfolio.css"
 // Images
 import beach from "./images/beach.png";
-import leftArrow from "./images/leftarrow.png";
-import rightArrow from "./images/rightArrow.png";
-
-
    
 export default function Portfolio() {
+
+    const ref = useRef(null)
+
+    const scroll = (scrollOffset) => {
+        ref.current.scrollLeft += scrollOffset;
+      };
 
     return (
         <>
         <section className="portfolioBackground">
-
             {/* Title */}
-            <div className="contents">
-                <div className="titleDiv"><h3 className="title">Portfolio</h3></div>
-            </div>
+            <div className="titleDiv"><h3 className="title">Portfolio</h3></div>
+
             {/* Left Arrow */}
-            <img src={leftArrow} alt="left arrow" className="arrow" style={{ left: '1vh'}}/>
+            <button 
+                className="arrow" 
+                onClick={() => scroll(-400)} 
+                style={{ left: '1vh'}}>
+                    &#8656;
+            </button>
+
+            {/* Right Arrow */}
+            <button 
+                className="arrow"
+                onClick={() => scroll(400)} 
+                style={{ right: '1vh'}} >
+                    &#8658;
+            </button>
 
             {/* Scrolling list of Projects */}
             <ImageList cols={1.1}
                 className="wrapper"
+                ref={ref}
             > 
                 <Projects /> 
             </ImageList>
-
-            {/* Right Arrow */}
-            <img src={rightArrow} alt="right arrow" className="arrow" style={{ right: '1vh'}}/>
 
             {/* Background Img */}
             <img src={beach} alt="beach" className="beach"/>
