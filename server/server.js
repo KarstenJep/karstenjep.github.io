0da5afbd24fa952bc093c6af91e6b8517a8417db
Karsten Jepsen <kjepsen86@gmail.com>
2021-12-01 15:07:04 -0600

    clean up from deployment dubugging

--- server/server.js
+++ server/server.js
@@ -18,10 +18,9 @@ app.use('/api/visitor', visitorRouter);
 app.use(express.static('build'));
 
 // App Set //
-const host = '0.0.0.0';
 const PORT = process.env.PORT || 5000;
 
 /** Listen * */
-app.listen(PORT, host, () => {
-  console.log(`Listening on port: ${PORT}, with host: ${host}`);
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}[]`);
 });
