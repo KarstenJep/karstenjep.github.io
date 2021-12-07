3ec855eaae62ff22e5681008278e105594eaec95
Karsten Jepsen <kjepsen86@gmail.com>
2021-12-07 15:21:13 -0600

    commented out back end. Deploying to gh-pages and will not be used

--- server/server.js
+++ server/server.js
@@ -1,26 +1,26 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const app = express();
 
-// Route includes
-const mailRouter = require('./mail.router');
-const visitorRouter = require('./visitor.router');
+// // Route includes
+// const mailRouter = require('./mail.router');
+// const visitorRouter = require('./visitor.router');
 
-// Body parser middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// // Body parser middleware
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
 
-/* Routes */
-app.use('/api/mail', mailRouter);
-app.use('/api/visitor', visitorRouter);
+// /* Routes */
+// app.use('/api/mail', mailRouter);
+// app.use('/api/visitor', visitorRouter);
 
-// Serve static files
-app.use(express.static('build'));
+// // Serve static files
+// app.use(express.static('build'));
 
-// App Set //
-const PORT = process.env.PORT || 5000;
+// // App Set //
+// const PORT = process.env.PORT || 5000;
 
-/** Listen * */
-app.listen(PORT, () => {
-  console.log(`Listening on port: ${PORT}[]`);
-});
+// /** Listen * */
+// app.listen(PORT, () => {
+//   console.log(`Listening on port: ${PORT}[]`);
+// });
--- server/visitor.router.js
+++ server/visitor.router.js
@@ -1,32 +1,32 @@
-const express = require('express');
-const router = express.Router();
-const pool = require('./pool')
+// const express = require('express');
+// const router = express.Router();
+// const pool = require('./pool')
 
-// GET list of all visitors from DB
-router.get('/', (req, res) => {
-  const query = `SELECT * FROM visitors ORDER BY "id" ASC`;
-  pool.query(query)
-    .then( result => {
-      res.send(result.rows);
-    })
-    .catch(err => {
-      console.log('ERROR: Get all visitors', err);
-      res.sendStatus(500)
-    })
-});
+// // GET list of all visitors from DB
+// router.get('/', (req, res) => {
+//   const query = `SELECT * FROM visitors ORDER BY "id" ASC`;
+//   pool.query(query)
+//     .then( result => {
+//       res.send(result.rows);
+//     })
+//     .catch(err => {
+//       console.log('ERROR: Get all visitors', err);
+//       res.sendStatus(500)
+//     })
+// });
 
-// POST new visitor to DB
-router.post('/', (req, res) => {
-  const nameQuery = `
-    INSERT INTO "visitors" ("name")
-    VALUES ($1);`
-  pool.query(nameQuery, [req.body.name])
-    .then(result => {
-    })
-    .catch(err => {
-      console.log(err);
-      res.sendStatus(500)
-    })
-})
+// // POST new visitor to DB
+// router.post('/', (req, res) => {
+//   const nameQuery = `
+//     INSERT INTO "visitors" ("name")
+//     VALUES ($1);`
+//   pool.query(nameQuery, [req.body.name])
+//     .then(result => {
+//     })
+//     .catch(err => {
+//       console.log(err);
+//       res.sendStatus(500)
+//     })
+// })
 
-module.exports = router;
+// module.exports = router;
